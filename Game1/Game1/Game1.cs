c8f2703538590306ef45992b7c7717e726059575
IndiegameGarden <info@indiegamegarden.com>
2016-08-21 22:59:45 +0200

    Added factory; messing with Nuget packages which doesn't work in a former xna project. Going to migrate to real Monogame project.

--- Game1/Game1/Game1.cs
+++ Game1/Game1/Game1.cs
@@ -21,12 +21,12 @@ using TreeSharp;
 namespace Game1
 {
     /// <summary>
-    /// Main game class acting as template for your own games based on TTengine.
-    /// Modify this class directly to start coding your game.
+    /// Main game class.
     /// </summary>
     public class Game1 : TTGame
     {
         public Game1Factory Factory;
+        public Entity IntroChannel, GameChannel;
 
         public Game1()
         {
@@ -51,6 +51,9 @@ namespace Game1
 
         protected override void LoadContent()
         {
+            IntroChannel = Game1Factory.CreateChannel(Color.Black);
+            GameChannel = Game1Factory.CreateChannel(Color.Black);
+
             var scr = MainChannel.GetComponent<ScreenComp>();
             scr.BackgroundColor = Color.White;
 
@@ -72,104 +75,6 @@ namespace Game1
 
             base.LoadContent();
         }                  
-    }
-
-    /// <summary>
-    /// Factory to create new game-specific entities.
-    /// For your own game, move it into a separate file.
-    /// </summary>
-    public class Game1Factory: TTFactory
-    {
-        private static Game1Factory _instance = null;
-        private Game1 _game;
-
-        private Game1Factory(Game1 game)
-        {
-            _game = game;
-        }
-
-        public static Game1Factory Instance
-        {
-            get
-            {
-                if (_instance == null)
-                    _instance = new Game1Factory(TTGame.Instance as Game1);
-                return _instance as Game1Factory;
-            }
-        }
-
-        protected Random rnd = new Random();
-
-        /// <summary>
-        /// create a ball Spritelet that can be scaled
-        /// </summary>
-        /// <param name="radius">the relative size scaling, 1 is normal</param>
-        /// <returns></returns>
-        public Entity CreateBall(double radius)
-        {
-            Entity e = CreateSpritelet("paul-hardman_circle-four");
-            e.AddComponent(new ScaleComp(radius));
-            return e;
-        }
-
-        /// <summary>
-        /// create an active ball with given position and random velocity and some weird (AI) behaviors
-        /// </summary>
-        /// <returns></returns>
-        public Entity CreateHyperActiveBall(Vector2 pos)
-        {
-            var ball = CreateBall(0.08f + 0.07f * (float)rnd.NextDouble());
-
-            // position and velocity set
-            ball.GetComponent<PositionComp>().Position = pos;
-            ball.GetComponent<VelocityComp>().Velocity = 0.2f * new Vector2((float)rnd.NextDouble() - 0.5f, (float)rnd.NextDouble() - 0.5f);
-
-            /*
-            // duration of entity
-            ball.AddComponent(new ExpiresComp(4 + 500 * rnd.NextDouble()));
-
-            // Behavior Tree AI
-            BTAIComp ai = new BTAIComp();
-            var randomWanderBehavior = new RandomWanderBehavior(1, 6);
-            ai.rootNode = new PrioritySelector(randomWanderBehavior);
-            ball.AddComponent(ai);
-
-            // Modifier to adapt scale
-            TTFactory.AddModifier(ball, ScaleModifierScript);
-
-            // another adapting scale with sine rhythm
-            var s = new SineFunction();
-            s.Frequency = 0.5;
-            s.Amplitude = 0.25;
-            s.Offset = 1;
-            TTFactory.AddModifier(ball, ScaleModifierScript, s);
-
-            // modifier to adapt rotation
-            TTFactory.AddModifier(ball, RotateModifierScript);
-
-            // set different time offset initially, per ball (for the modifiers)
-            ball.GetComponent<ScriptComp>().SimTime = 10 * rnd.NextDouble();
-            */
-
-            ball.Refresh();
-            return ball;
-
-        }
-
-        public Entity CreateMovingTextlet(Vector2 pos, string text)
-        {
-            var t = CreateTextlet(text);
-            t.GetComponent<PositionComp>().Position = pos;
-            t.GetComponent<DrawComp>().DrawColor = Color.Black;
-            t.GetComponent<VelocityComp>().Velocity = 0.2f * new Vector2( (float)rnd.NextDouble() - 0.5f, (float)rnd.NextDouble() - 0.5f);
-            t.GetComponent<ScaleComp>().Scale = 0.5;
-            return t;
-        }
-
-        public void RotateModifierScript(ScriptContext ctx, double value)
-        {
-            ctx.Entity.GetComponent<DrawComp>().DrawRotation = (float)value;
-        }
     }
 
 }
---
+++ Game1/Game1/Game1Factory.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+using TTengine.Core;
+using TTengine.Comps;
+using TTengine.Behaviors;
+using TTengine.Modifiers;
+using TTengine.Util;
+
+using Artemis;
+using Artemis.Interface;
+using TreeSharp;
+
+namespace Game1
+{
+    public class Game1Factory: TTFactory
+    {
+        private static Game1Factory _instance = null;
+        private Game1 _game;
+
+        private Game1Factory(Game1 game)
+        {
+            _game = game;
+        }
+
+        public static Game1Factory Instance
+        {
+            get
+            {
+                if (_instance == null)
+                    _instance = new Game1Factory(TTGame.Instance as Game1);
+                return _instance as Game1Factory;
+            }
+        }
+
+        protected Random rnd = new Random();
+
+        /// <summary>
+        /// create a ball Spritelet that can be scaled
+        /// </summary>
+        /// <param name="radius">the relative size scaling, 1 is normal</param>
+        /// <returns></returns>
+        public Entity CreateBall(double radius)
+        {
+            Entity e = CreateSpritelet("paul-hardman_circle-four");
+            e.AddComponent(new ScaleComp(radius));
+            return e;
+        }
+
+        /// <summary>
+        /// create an active ball with given position and random velocity and some weird (AI) behaviors
+        /// </summary>
+        /// <returns></returns>
+        public Entity CreateHyperActiveBall(Vector2 pos)
+        {
+            var ball = CreateBall(0.08f + 0.07f * (float)rnd.NextDouble());
+
+            // position and velocity set
+            ball.GetComponent<PositionComp>().Position = pos;
+            ball.GetComponent<VelocityComp>().Velocity = 0.2f * new Vector2((float)rnd.NextDouble() - 0.5f, (float)rnd.NextDouble() - 0.5f);
+
+            /*
+            // duration of entity
+            ball.AddComponent(new ExpiresComp(4 + 500 * rnd.NextDouble()));
+
+            // Behavior Tree AI
+            BTAIComp ai = new BTAIComp();
+            var randomWanderBehavior = new RandomWanderBehavior(1, 6);
+            ai.rootNode = new PrioritySelector(randomWanderBehavior);
+            ball.AddComponent(ai);
+
+            // Modifier to adapt scale
+            TTFactory.AddModifier(ball, ScaleModifierScript);
+
+            // another adapting scale with sine rhythm
+            var s = new SineFunction();
+            s.Frequency = 0.5;
+            s.Amplitude = 0.25;
+            s.Offset = 1;
+            TTFactory.AddModifier(ball, ScaleModifierScript, s);
+
+            // modifier to adapt rotation
+            TTFactory.AddModifier(ball, RotateModifierScript);
+
+            // set different time offset initially, per ball (for the modifiers)
+            ball.GetComponent<ScriptComp>().SimTime = 10 * rnd.NextDouble();
+            */
+
+            ball.Refresh();
+            return ball;
+
+        }
+
+        public Entity CreateMovingTextlet(Vector2 pos, string text)
+        {
+            var t = CreateTextlet(text);
+            t.GetComponent<PositionComp>().Position = pos;
+            t.GetComponent<DrawComp>().DrawColor = Color.Black;
+            t.GetComponent<VelocityComp>().Velocity = 0.2f * new Vector2((float)rnd.NextDouble() - 0.5f, (float)rnd.NextDouble() - 0.5f);
+            t.GetComponent<ScaleComp>().Scale = 0.5;
+            return t;
+        }
+
+        public void RotateModifierScript(ScriptContext ctx, double value)
+        {
+            ctx.Entity.GetComponent<DrawComp>().DrawRotation = (float)value;
+        }
+    }
+}
